b200d793ed1ffae8e8a4b274d5c10ed5de87d9ae
Vitor Cotta <vitor@tecnofisc.com.br>
2025-05-24 20:01:27 -0300

    test(core): adicionado teste unitário para CategoryService

---
+++ Budget.Tests/Services/CategoryServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Budget.Core.Interfaces;
+using Budget.Core.Services;
+using Moq;
+
+namespace Budget.Tests.Services;
+
+public class CategoryServiceTests
+{
+    [Fact]
+    public async Task AddAsync_ShouldThrow_WhenNameIsEmpty()
+    {
+        var repo = new Mock<ICategoryRepository>();
+        var service = new CategoryService(repo.Object);
+
+        var exception = await Assert.ThrowsAsync<ArgumentException>(() =>
+            service.AddAsync(""));
+
+        Assert.Equal("Category name is required.", exception.Message);
+    }
+
+    [Fact]
+    public async Task AddAsync_ShouldCallRepository_WhenNameIsValid()
+    {
+        var repo = new Mock<ICategoryRepository>();
+        var service = new CategoryService(repo.Object);
+
+        await service.AddAsync("Utilities");
+
+        repo.Verify(r => r.AddAsync(It.IsAny<Budget.Core.Entities.Category>()), Times.Once);
+    }
+}
